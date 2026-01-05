c738274cca17fdbdf94fdf3900dd94fbafc5725e
Pavel <yashchenkodev@gmail.com>
2026-01-05 23:44:18 +0200

    feat: add components to app

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
