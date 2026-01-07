14908939339e0468c7961f2fcaec8cb1eb48016d
Pavel <yashchenkodev@gmail.com>
2026-01-07 18:37:13 +0200

    feat: add server request to mockapi for items render

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
 
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://695e6b052556fd22f678652b.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,8 +28,8 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
+            {items.map((pizzas) => (
+              <PizzaBlock key={pizzas.id} {...pizzas} />
             ))}
           </div>
         </div>
